3827d68569e584f655d5967a4efb521536d48164
Guido Brand <g.brand@inpsyde.com>
2015-09-30 16:08:43 +0200

    GPL added

--- wp-ease.php
+++ wp-ease.php
@@ -1,14 +1,35 @@
 <?php
-/*
-Plugin Name: WP-Ease // ixcellent
-Plugin URI: http://ixcellent.de/
-Description: Easing the work with WordPress
-Version: 0.1
-Author: Guido Brand
-Author URI: http://ixcellent.de/
-Text Domain: ixc-ease
-Domain Path: /languages
-*/
+/**
+ * Plugin Name: WP-Ease // ixcellent
+ * Plugin URI: http://ixcellent.de/
+ * Description: Easing the work with WordPress
+ * Version: 0.1
+ * Author: Guido Brand
+ * Author URI: http://ixcellent.de/
+ * Text Domain: ixc-ease
+ * Domain Path: /languages
+ * Network: true
+ * License: GPLv3
+ * License URI: http://www.gnu.org/licenses/gpl-3.0
+ */
+
+/**
+ *	Copyright (C) 2012-2015 ixcellent (email: info@iixcellent.de)
+ *
+ *	This program is free software; you can redistribute it and/or
+ *	modify it under the terms of the GNU General Public License
+ *	as published by the Free Software Foundation; either version 2
+ *	of the License, or (at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program; if not, write to the Free Software
+ *	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 
 // Remove unused header information and links
 add_action('init', 'removeheadlinks');
